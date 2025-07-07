dca9fbab49b1d9f47ecc886e9e9b36a51719b06d
Sondre Bolland <sondre.bolland@uib.no>
2025-07-07 03:53:09 +0200

    Remove festplay from urls

--- festival_playlist_generator/urls.py
+++ festival_playlist_generator/urls.py
@@ -21,5 +21,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/', include('api.urls')),
     path('spotify/', include('spotify.urls')),
-    path('festplay/', include('frontend.urls'))
+    path('', include('frontend.urls'))
 ]
