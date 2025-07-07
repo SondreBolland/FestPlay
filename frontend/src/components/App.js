0edd6c5daff5e7802128d3b0ce5f0598af13087a
Sondre Bolland <sondre.bolland@uib.no>
2025-07-07 03:06:22 +0200

    route update

--- frontend/src/components/App.js
+++ frontend/src/components/App.js
@@ -1,23 +1,19 @@
 import React, { Component } from "react";
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import { render } from "react-dom";
 import HomePage from "./HomePage";
 import PlaylistConfig from "./PlaylistConfig";
 import AddArtistPage from "./AddArtistPage";
 
 export default class App extends Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
       <Router basename="/festplay">
-        <Routes>
-          <Route path="/" element={<HomePage />} />
-          <Route path="/playlist-config" element={<PlaylistConfig />} />
-          <Route path="/add-artist" element={<AddArtistPage />} />
-        </Routes>
+        <Switch>
+          <Route exact path="/" component={HomePage} />
+          <Route path="/playlist-config" component={PlaylistConfig} />
+          <Route path="/add-artist" component={AddArtistPage} />
+        </Switch>
       </Router>
     );
   }
