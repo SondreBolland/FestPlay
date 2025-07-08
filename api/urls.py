824cd261287082a9e4dda38dd3932396c23287c3
Sondre Bolland <sondre.bolland@uib.no>
2025-07-08 03:47:04 +0200

    Add festplay/ to all api urls

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,12 @@
-from django.urls import path
+from django.urls import path, include
 from .views import *
 
 urlpatterns = [
+    path('festplay/', include([
     path('artists/', ArtistView.as_view()),
     path('add-artist/', CreateArtistView.as_view()),
     path('top-songs/', TopSongListView.as_view()),
     path("artist-search/", SearchArtistView.as_view()),
-    path("artists/<int:pk>/", ArtistDetailView.as_view(), name="delete-artist"),
+    path("artists/<int:pk>/", ArtistDetailView.as_view(), name="delete-artist")
+]))
 ]
